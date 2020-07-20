528102369714c5ad901f06bee11b49e8c0a0577c
Jyoti Patel <jyotipatel152@gmail.com>
2020-07-19 22:17:28 -0400

    Move gesture import to the top as mentioned in their docs.

--- App.js
+++ App.js
@@ -2,12 +2,11 @@
  * @format
  * @flow strict-local
  */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
 import HomeScreen from './src/HomeScreen'
 import StoryScreen from './src/StoryScreen'
-import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
