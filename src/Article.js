b5fd5d23f4cd71579931d2b92cd1bc0bed0be79a
Jyoti Patel <jyotipatel152@gmail.com>
2020-07-20 12:47:03 -0400

    Show loading indicator in article's webview.

--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { ActivityIndicator } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 class Article extends Component {
   render() {
-    return <WebView source={{ uri: this.props.url }}/>;
+    return (
+      <WebView
+        source={{ uri: this.props.url }}
+        startInLoadingState={true}
+        renderLoading={() =>
+          <ActivityIndicator
+            color = '#bc2b78'
+            size = "large"
+            hidesWhenStopped={true}
+          />}
+      />
+    );
   }
 }
 
