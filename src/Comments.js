5776ee3507b33f40b0cfbe662b12f59cac92daf4
Jyoti Patel <jyotipatel152@gmail.com>
2020-07-21 11:40:13 -0400

    Make comments collapsable.
    Using comments state as open or close, longpress on comments collapses the
    comment and onpress opens it again

--- src/Comments.js
+++ src/Comments.js
@@ -2,35 +2,86 @@ import React, { Component } from 'react';
 import { Text, View, FlatList } from 'react-native';
 
 class Comments extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: true
+    };
+  }
+
   stripHTML = (x) => {
     const regex = /(<([^>]+)>)/ig;
     return x.replace(regex, '');
   }
 
+  onLongPressComment = () => {
+    if(this.state.isOpen){
+      this.setState({
+        isOpen: false
+      });
+    }
+  }
+  onPressComment = () => {
+    if(!this.state.isOpen){
+      this.setState({
+        isOpen: true
+      });
+    }
+  }
+
+  createCommentText = (comment) => {
+    const stripped = this.stripHTML(comment);
+    if(this.state.isOpen){
+      return (
+        <Text 
+          onLongPress={this.onLongPressComment} 
+          onPress={this.onPressComment}>
+            {stripped}
+        </Text>
+      );
+    }
+
+    return (
+      <Text
+        numberOfLines={2}
+        ellipsizeMode='tail'
+        onLongPress={this.onLongPressComment}
+        onPress={this.onPressComment}>
+          {stripped}
+      </Text>
+    );
+  }
   render() {
     const renderComment = ({ item }) => {
-      // Strip html
       // TODO: Figure out how to render HTML properly. Multiple WebViews don't work!
-      const stripped = this.stripHTML(item.content);
-
+      let text = this.createCommentText(item.content);
+      let children_comments = null;
+      if(this.state.isOpen){
+        children_comments = <Comments comment={item} />;
+      }
       return (
         <View style={{marginLeft: 16, borderLeftWidth: 1, paddingLeft: 4}}>
-          <Text>{stripped}</Text>
-          <Comments comment={item} />
+          {text}
+          {children_comments}
         </View>
       );
     };
-
-    const stripped = this.stripHTML(this.props.comment.content);
-
-    return (
-      <View style={{margin: 8}}>
-        <Text>{stripped}</Text>
+    
+    let text = this.createCommentText(this.props.comment.content);
+    let comments_list = null;
+    if(this.state.isOpen){
+      comments_list = (
         <FlatList
           data={this.props.comment.comments}
           renderItem={renderComment}
           keyExtractor={c => c.id}
-        />
+        />);
+    }
+    return (
+      <View style={{margin: 8}}>
+        {text}
+        {comments_list}
       </View>  
     );
   }
